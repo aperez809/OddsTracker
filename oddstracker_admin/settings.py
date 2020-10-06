9078afdba074241fc4f0bc44254283131a5ae7a3
aperez809 <32823645+aperez809@users.noreply.github.com>
2020-10-06 13:22:15 -0700

    adding EB server to allowed_host list

--- oddstracker_admin/settings.py
+++ oddstracker_admin/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '7^&exvy6ald9b3pm8x@xejf!y+bl444x*!naw++^blgd*y6!zd'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['oddstracker-env.eba-tjfu7yri.us-east-2.elasticbeanstalk.com']
 
 
 # Application definition

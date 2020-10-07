42aa5aa7f64846d988419822a68673c13c2e1692
aperez809 <32823645+aperez809@users.noreply.github.com>
2020-10-07 10:26:44 -0700

    improved testing with fixtures for mock data

--- oddstracker/tests.py
+++ oddstracker/tests.py
@@ -1,105 +1,123 @@
-from collections import OrderedDict
-
-from django.test import TestCase
-
 # Create your tests here.
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
 from oddstracker.models import Game
 import json
-from pprint import pprint
 
 
 class GameTests(APITestCase):
-    sports_data = {'name': 'Football'}
-    regions_data = {'name': 'US'}
-    leagues_data = {'name': 'NFL'}
-    source_data_1 = {'name': 'Caesars'}
-    source_data_2 = {'name': 'DraftKings'}
-    now = '2020-10-05T18:37:00.744747Z'
+    fixtures = [
+        'DBAdmin_TestData.json',
+        'Game_TestData.json'
+    ]
 
-    odds_data_1 = {'game':1, 'time_recorded':now, 'source': 1, 'mkt_type':'H2H', 'team_a_value': 150, 'team_b_value': -150}
-    odds_data_2 = {'game':1, 'time_recorded':now, 'source': 2, 'mkt_type':'H2H', 'team_a_value': 150, 'team_b_value': -150}
+    # Number of Games in DB before testing
+    STARTING_NUM_GAMES = 2
 
+    # URL for getting list of or creating Games
+    games_listcreate_url = reverse('games-list')
+
+    # Mock Game data
     game_data = {
         'team_a': 'Washington Football Team',
         'team_b': 'Dallas Cowboys',
-        'game_time': now,
+        'game_time': '2020-10-05T18:37:00.744747Z',
         'sport': 1,
         'region': 1,
         'league': 1
     }
 
-    sports_url = reverse('sports-list')
-    regions_url = reverse('regions-list')
-    leagues_url = reverse('leagues-list')
-    sources_url = reverse('sources-list')
-    odds_url = reverse('odds-list')
-
-    def setup(self):
-        """
-        Create mock data in temp db to test requests
-        """
-        self.client.post(self.sports_url, self.sports_data, format='json')
-        self.client.post(self.regions_url, self.regions_data, format='json')
-        self.client.post(self.leagues_url, self.leagues_data, format='json')
-        self.client.post(self.sources_url, self.source_data_1, format='json')
-        self.client.post(self.sources_url, self.source_data_2, format='json')
-
-
     def test_create_game(self):
         """
         Ensure we can create a new Game object.
         """
-        self.setup()
-        url = reverse('games-list')
-        response = self.client.post(url, data=self.game_data, format='json')
+        response = self.client.post(self.games_listcreate_url, data=self.game_data, format='json')
+        fmt_res = json.loads(response.content)
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(Game.objects.count(), 1)
-        self.assertEqual(Game.objects.get().team_a, 'Washington Football Team')
+        self.assertEqual(Game.objects.count(), self.STARTING_NUM_GAMES + 1)
+        self.assertEqual(Game.objects.get(id=fmt_res['id']).team_a, 'Washington Football Team')
+
+    def test_create_dupe_game(self):
+        """
+        Ensure we can NOT create a duplicate Game object.
+        """
+        response_1 = self.client.post(self.games_listcreate_url, data=self.game_data, format='json')
+        response_2 = self.client.post(self.games_listcreate_url, data=self.game_data, format='json')
+
+        fmt_res = json.loads(response_1.content)
+
+        self.assertEqual(response_1.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response_2.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(Game.objects.count(), self.STARTING_NUM_GAMES + 1)
+        self.assertEqual(Game.objects.get(id=fmt_res['id']).team_a, 'Washington Football Team')
 
     def test_get_games(self):
-        self.setup()
-        url = reverse('games-list')
-        self.client.post(url, data=self.game_data, format='json')
-        self.client.post(url, data=self.game_data, format='json')
-
-        response = self.client.get(url)
+        """
+        Ensure we can retrieve list of Games
+        """
+        response = self.client.get(self.games_listcreate_url, format='json')
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(Game.objects.count(), 2)
+        self.assertEqual(Game.objects.count(), self.STARTING_NUM_GAMES)
 
     def test_get_game(self):
-        self.setup()
-        url = reverse('games-list')
-        self.client.post(url, data=self.game_data, format='json')
-        response = self.client.get(f'{url}/1', format='json')
+        """
+        Ensures that we can retrieve a given Game by ID
+        """
+        create_res = self.client.post(self.games_listcreate_url, data=self.game_data, format='json')
+        fmt_create_res = json.loads(create_res.content)
+
+        get_url = reverse('games-detail', args=[fmt_create_res['id']])
+        response = self.client.get(get_url, format='json')
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(json.loads(response.content),
                          {
-                             'id': 1,
+                             'id': fmt_create_res['id'],
                              'team_a': 'Washington Football Team',
                              'team_b': 'Dallas Cowboys',
-                             'game_time': self.now,
+                             'game_time': '2020-10-05T18:37:00.744747Z',
                              'sport': 1,
                              'region': 1,
                              'league': 1
-                         }
-                         )
+                         })
+
+    def test_delete_game(self):
+        """
+        Ensures that we can properly delete a Game
+        #TODO Determine if this should be a hard or soft delete. Current test is hard delete.
+        """
+        create_res = self.client.post(self.games_listcreate_url, data=self.game_data, format='json')
+        fmt_create_res = json.loads(create_res.content)
+        self.assertEqual(Game.objects.count(), self.STARTING_NUM_GAMES + 1)
+
+        del_url = reverse('games-detail', args=[fmt_create_res['id']])
+        response = self.client.delete(del_url, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Game.objects.count(), self.STARTING_NUM_GAMES)
+
+
+class OddsTests(APITestCase):
+    pass
+
+
+class GameOddsTests(APITestCase):
+    fixtures = [
+        'DBAdmin_TestData.json',
+        'GameOdds_TestData.json'
+    ]
 
     def test_get_game_with_odds(self):
-        self.setup()
-        url = reverse('games-list')
-        self.client.post(url, data=self.game_data, format='json')
-        self.client.post(self.odds_url, self.odds_data_1, format='json')
-        self.client.post(self.odds_url, self.odds_data_2, format='json')
-        response = self.client.get(f'{url}/1/odds', format='json')
+        """
+        Ensures a game and its associated odds can be retrieved
+        """
+        gameodds_retrieve_url = reverse('games-odds-detail', args=[1])
+        response = self.client.get(gameodds_retrieve_url, format='json')
+        fmt_res = json.loads(response.content)
 
-        formatted_response = json.loads(response.content)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertIsNotNone(formatted_response)
-        self.assertEqual(len(formatted_response['odds']), 2)
-
+        self.assertIsNotNone(fmt_res)
+        self.assertEqual(len(fmt_res['odds']), 2)
